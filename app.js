b689484e941b275ff67a022257e73e44de5ba0f1
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-25 20:09:23 +0530

    Added array to add new items in list

--- app.js
+++ app.js
@@ -5,46 +5,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", function (req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error current day is equal to" + currentDay);
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     });
 
 });
 
+app.post("/", function (req, res) {
+
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+});
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("server started at 3000!!!");
